46068f016543e4972c20127668e09909a9fcb808
Sascha Kim <saschakim@Saschas-MacBook-Pro.local>
2019-06-17 16:43:24 +1000

    inital commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.get
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Sever started on port ${PORT}`));
